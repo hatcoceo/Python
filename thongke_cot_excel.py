a2e12dbb85c9cbdc9d1753e34beb3ae751faa47c
hatcoceo <39942640+hatcoceo@users.noreply.github.com>
2024-07-31 10:22:44 +0700

    Update thongke_cot_excel.py

--- thongke_cot_excel.py
+++ thongke_cot_excel.py
@@ -16,26 +16,3 @@ thang_counts = df['Tháng'].value_counts()
 print(ngay_counts)
 print(thu_counts)
 print(thang_counts)
-'''
-import pandas as pd
-import unicodedata
-
-# Hàm để chuẩn hóa chuỗi ký tự và chuyển về chữ thường
-def normalize_string(s):
-    if isinstance(s, float):  # Kiểm tra nếu s là float (có thể là NaN)
-        return ''
-    return unicodedata.normalize('NFKC', s.strip().lower())
-
-# Đọc dữ liệu từ tệp Excel
-file_path = 'ngan_sach_nhanh_chong_2024.xlsx'
-df = pd.read_excel(file_path)
-
-# Chuyển đổi tất cả các giá trị trong cột 'Days' sang chuỗi và chuẩn hóa
-df['TK'] = df['TK'].astype(str).apply(normalize_string)
-
-# Đếm số lần xuất hiện của mỗi ngày trong tuần
-day_counts = df['TK'].value_counts()
-
-# In kết quả
-print(day_counts)
-'''
