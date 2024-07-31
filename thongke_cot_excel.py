c98e87be76070ef785ceafadddcf835488289dc6
hatcoceo <39942640+hatcoceo@users.noreply.github.com>
2024-07-31 10:25:51 +0700

    Update thongke_cot_excel.py

--- thongke_cot_excel.py
+++ thongke_cot_excel.py
@@ -5,7 +5,7 @@ import pandas as pd
 file_path = 'ngan_sach_nhanh_chong_2023_1.xlsx'
 df = pd.read_excel(file_path)
 
-# Đếm số lần xuất hiện của mỗi ngày trong tuần
+# Đếm số lần xuất hiện của mỗi thứ , ngày, tháng trong tuần
 ngay_counts = df['Ngày 2'].value_counts()
 thu_counts = df['Thứ'].value_counts()
 thang_counts = df['Tháng'].value_counts()
