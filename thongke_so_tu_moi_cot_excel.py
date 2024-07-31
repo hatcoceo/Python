5da57ab47d675b3db2b1ee58343fba7ad0b3428a
hatcoceo <39942640+hatcoceo@users.noreply.github.com>
2024-07-31 17:52:46 +0700

    Update

---
+++ thongke_so_tu_moi_cot_excel.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# Đọc tệp Excel
+df = pd.read_excel('Bài đăng.xlsx')
+
+# Giả sử cột bạn muốn thống kê có tên là 'nội dung''
+# Tạo một cột mới để lưu số lượng từ
+df['SoLuongTu'] = df['nội dung'].apply(lambda x: len(str(x).split()))
+
+# In kết quả
+print(df[['nội dung', 'SoLuongTu']])
+
+# Lưu kết quả ra tệp Excel mới nếu cần
+df.to_excel('ket_qua_thong_ke.xlsx', index=False)
